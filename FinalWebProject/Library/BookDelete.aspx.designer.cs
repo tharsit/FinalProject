aefb9285f6997f63d14787ead9fd39bd9e0d10ec
Htet Myat Aung <htetmyataungzerozerofour@gmail.com>
2023-09-05 21:16:03 +0630

    added deleting

--- FinalWebProject/Library/BookDelete.aspx.designer.cs
+++ FinalWebProject/Library/BookDelete.aspx.designer.cs
@@ -58,13 +58,13 @@ namespace FinalWebProject.Library {
         protected global::System.Web.UI.WebControls.Button btnSearch;
         
         /// <summary>
-        /// GridView1 control.
+        /// BookTable control.
         /// </summary>
         /// <remarks>
         /// Auto-generated field.
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.GridView GridView1;
+        protected global::System.Web.UI.WebControls.GridView BookTable;
         
         /// <summary>
         /// sqlBookTable control.
